407b80b2eee4ab08813944c561a1551017799dc0
VIOLET-N <69731229+VIOLET-N@users.noreply.github.com>
2020-11-25 11:30:30 +0800

    1

--- redbag/src/main/java/com/neusoft/redbag/LuckMoneyController.java
+++ redbag/src/main/java/com/neusoft/redbag/LuckMoneyController.java
@@ -3,8 +3,11 @@ package com.neusoft.redbag;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
@@ -15,5 +18,12 @@ public class LuckMoneyController {
     @GetMapping("/list")
     public List<LuckMoney> list(){
         return repository.findAll();
+    }
+    @PostMapping("/post")
+    public LuckMoney postRedBag(@RequestParam(value = "producer", required = true) String producer, @RequestParam(value = "money", required = true) BigDecimal money){
+        LuckMoney luckMoney = new LuckMoney();
+        luckMoney.setMoney(money);
+        luckMoney.setProducer(producer);
+        return repository.save(luckMoney);
     }
 }
